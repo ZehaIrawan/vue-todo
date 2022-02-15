395b68a96349dc473eabdeb59caebbd039d48ac7
Zeha Irawan <zehairawan@gmail.com>
2022-02-15 20:49:28 +0800

    map state

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,28 @@
-<script setup>
+<script>
 import TodoItem from "@/components/TodoItem.vue";
 
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          name: "Learn Vue",
+          done: false,
+        },
+        {
+          name: "Buy Eggs",
+          done: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
   <TodoItem />
+  <li v-for="item in todos">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.done" />
+  </li>
 </template>
