97a37460af91a097a0450c7c34eeaf39cb5c9e80
Zeha Irawan <zehairawan@gmail.com>
2022-02-16 11:17:23 +0800

    remove todo feature

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,10 @@
 <script>
-import TodoItem from "@/components/TodoItem.vue";
+// import TodoItem from "@/components/TodoItem.vue";
 
 export default {
   data() {
     return {
+      currentTodo: "",
       todos: [
         {
           name: "Learn Vue",
@@ -16,13 +17,29 @@ export default {
       ],
     };
   },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        name: this.currentTodo,
+        done: false,
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
 };
 </script>
 
 <template>
-  <TodoItem />
-  <li v-for="item in todos">
+  <!-- <TodoItem /> -->
+  <li v-for="item in todos" :key="item.name">
     {{ item.name }}
     <input type="checkbox" v-model="item.done" />
+    <button @click="removeTodo">X</button>
   </li>
+
+  <input v-model="currentTodo" type="text" placeholder="Buy milk" />
+  <button @click="addTodo">Add Task</button>
 </template>
